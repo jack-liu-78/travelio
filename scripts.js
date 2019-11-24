cf3e3a5085c5fb4425fdd596b3fa9402c14123b3
Jennifer Chen <jenn.chenn93@gmail.com>
2019-11-23 22:11:25 -0500

    commenting code

--- scripts.js
+++ scripts.js
@@ -79,9 +79,8 @@ function saveInputParams(n, start, end) {
     }
     if (setPerson) {
         people.push({name: n, aStart: start, aEnd: end});
-        // ws.send(people)
+        ws.send(people)
     }
-
     // ws.send(JSON.stringify({'type': 'userList', 'data': people}))
     console.log(people)
 }
